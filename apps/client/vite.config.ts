f624892404cb7144aad9e0dc760b76a936e3375f
LuedanDev@ubuntu <atencia17@gmail.com>
2024-03-24 10:41:06 -0500

    inicializando repo

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        changeOrigin: true,
+        target: 'http://localhost:3001',
+      },
+      
+    }
+  }
 })
